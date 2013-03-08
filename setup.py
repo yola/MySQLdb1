bdcd16ef339011f8b6ab07fcce5e5ea37d1bf7f6
Stefano Rivera <stefano@rivera.za.net>
2013-03-08 16:05:54 +0200

    Don't use distribute_setup, the version of distribute we have is just fine, thanks

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import os
 import sys
 
-from distribute_setup import use_setuptools
-use_setuptools()
+#from distribute_setup import use_setuptools
+#use_setuptools()
 from setuptools import setup, Extension
 
 if not hasattr(sys, "hexversion") or sys.hexversion < 0x02040000:
